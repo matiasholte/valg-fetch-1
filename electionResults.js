6bac779b90fbaa53154ff8eaf2b3bbeb918654f9
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2017-09-03 19:35:50 +0200

    legg til rette for lagring av fleire val

--- electionResults.js
+++ electionResults.js
@@ -1,8 +1,12 @@
 let fetch = require("node-fetch");
 
-async function storeNewResults(electionPath, database) {
+async function storeNewResults(
+  electionPath,
+  database,
+  baseElectionPath = undefined
+) {
   let fetched = await fetchResult(electionPath);
-  await storeResults(fetched, database);
+  await storeResults(fetched, database, baseElectionPath || electionPath);
 }
 
 async function fetchResult(electionPath) {
@@ -16,39 +20,64 @@ async function fetchResult(electionPath) {
   return body;
 }
 
-async function storeResults(fetched, database) {
+async function storeResults(fetched, database, baseElectionPath) {
   if (
-    await newResult(fetched.id.nr, fetched.tidspunkt.rapportGenerert, database)
+    await newResult(
+      fetched.id.nr,
+      fetched.id.nivaa,
+      fetched.tidspunkt.rapportGenerert,
+      database
+    )
   ) {
     console.log(`NEW RESULTS: ${fetched.id.navn}`);
-    await storeResult(fetched, database);
+    await storeResult(fetched, database, baseElectionPath);
   } else {
     console.log(`NO CHANGE: ${fetched.id.navn}`);
   }
   for (let relatedResult of fetched["_links"].related) {
     let relatedPath = electionPathOfRelated(relatedResult);
     if (
-      await newResult(relatedResult.nr, relatedResult.rapportGenerert, database)
+      await newResult(
+        relatedResult.nr,
+        LEVELS[LEVELS.indexOf(fetched.id.nivaa) + 1],
+        relatedResult.rapportGenerert,
+        database,
+        baseElectionPath
+      )
     ) {
-      await storeNewResults(relatedPath, database);
+      await storeNewResults(relatedPath, database, baseElectionPath);
     }
   }
 }
 
-function dbPathOfFetched(nr, rapportGenerert) {
-  const DB_ROOT = "/results";
-  return `${DB_ROOT}/${nr}/${rapportGenerert}`;
+const LEVELS = ["land", "fylke", "kommune", "bydel", "stemmekrets"];
+
+function dbPathOfFetched(nr, nivaa, rapportGenerert, baseElectionPath) {
+  return `${baseElectionPath}/${nivaa}-${nr}/${rapportGenerert}`;
 }
 
-async function newResult(nr, rapportGenerert, database) {
-  let dbRef = database.ref(dbPathOfFetched(nr, rapportGenerert));
+async function newResult(
+  nr,
+  nivaa,
+  rapportGenerert,
+  database,
+  baseElectionPath
+) {
+  let dbRef = database.ref(
+    dbPathOfFetched(nr, nivaa, rapportGenerert, baseElectionPath)
+  );
   let stored = (await dbRef.once("value")).val();
   return !stored;
 }
 
-async function storeResult(fetched, database) {
+async function storeResult(fetched, database, baseElectionPath) {
   let dbRef = database.ref(
-    dbPathOfFetched(fetched.id.nr, fetched.tidspunkt.rapportGenerert)
+    dbPathOfFetched(
+      fetched.id.nr,
+      fetched.id.nivaa,
+      fetched.tidspunkt.rapportGenerert,
+      baseElectionPath
+    )
   );
   await dbRef.set(fetched);
 }
