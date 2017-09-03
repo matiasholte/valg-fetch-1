0952f274e6d6e0e47129f150513556aa31554a64
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2017-09-03 10:40:41 +0200

    rekursiv hent data

--- electionResults.js
+++ electionResults.js
@@ -1,9 +1,14 @@
 let fetch = require("node-fetch");
 
-const API_ROOT = "api/2015/ko";
+const API_ROOT = "/api/2015/ko";
 
 async function topLevelResults() {
-  let result = await fetch(`http://valgresultat.no/${API_ROOT}`);
+  return await fetchResult(API_ROOT);
+}
+
+async function fetchResult(path) {
+  console.warn(`[fetching: ${path}]`);
+  let result = await fetch(`http://valgresultat.no${path}`);
   if (result.status !== 200) {
     throw new Error(`Unexpected status code: ${result.status}`);
   }
@@ -12,15 +17,43 @@ async function topLevelResults() {
 }
 
 async function storeResults(results, database) {
-  let topLevelRef = database.ref(API_ROOT);
-  let topLevelData = (await topLevelRef.once("value")).val();
+  await _storeResults(results, database, API_ROOT);
+}
+
+async function _storeResults(fetchedResults, database, path) {
+  let dbRef = database.ref(path);
+  let storedData = (await dbRef.once("value")).val();
   if (
-    !topLevelData ||
-    (topLevelData.rapportGenerert &&
-      topLevelData.rapportGenerert !== results.rapportGenerert)
+    !storedData ||
+    (storedData.rapportGenerert &&
+      storedData.rapportGenerert !== fetchedResults.rapportGenerert)
   ) {
-    console.log("* landsresultat");
-    await topLevelRef.set(results);
+    console.log(`* ${fetchedResults.id.navn}`);
+    await dbRef.set(fetchedResults);
+  }
+  let relatedResultsWithNewData = fetchedResults[
+    "_links"
+  ].related.filter(async related => {
+    let relatedRef = database.ref(related.hrefNavn);
+    let relatedData = (await relatedRef.once("value")).val();
+    return (
+      !relatedData ||
+      (relatedData.rapportGenerert &&
+        relatedData.rapportGenerert !== related.rapportGenerert)
+    );
+  });
+  await storeRelated(relatedResultsWithNewData, database);
+}
+
+async function storeRelated(related, database) {
+  for (let rel of related) {
+    let apiPath = `/api${rel.hrefNavn}`;
+    let fetchedResults = await fetchResult(apiPath);
+
+    let dbPath = `${apiPath.slice(0, 12)}${apiPath
+      .slice(12)
+      .replace(/\//g, "/underordnet/")}`;
+    await _storeResults(fetchedResults, database, dbPath);
   }
 }
 
