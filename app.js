7b6a143eb6be3f142aa60b18ca8aa97b2ae5aa89
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2017-09-05 09:23:49 +0200

    lag kommandolineverktoy

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 // @flow
 
+let cmdParser = require("optimist");
 let electionResults = require("./electionResults");
 let database = require("./database");
 
-const ELECTION_PATH = "/2017/st";
+let argv = cmdParser
+  .usage(
+    "Fetch Norwegian election results and put them in a Firebase instance.\nUsage:$0"
+  )
+  .demand(1)
+  .options("e", { alias: "electionPath", default: "/2017/st" }).argv;
 
-async function run() {
+const command = argv._[0];
+
+if (command === "update") {
+  update(argv.e);
+}
+if (command === "clear") {
+  clear(argv.e);
+}
+if (command === "init") {
+  init(argv.e);
+}
+
+async function update(electionPath) {
   let db = await database.initialize();
   console.log("initialized database");
-  await electionResults.storeNewResults(ELECTION_PATH, db);
+  await electionResults.storeNewResults(electionPath, db);
   console.log("stored results from api");
-  console.log("done, exiting");
   process.exit(0);
 }
 
-run();
+async function clear(electionPath) {
+  let db = await database.initialize();
+  console.log("initialized database");
+  await db.ref(electionPath).remove();
+  console.log("deleted election", electionPath);
+  process.exit(0);
+}
+
+async function init(electionPath) {
+  let db = await database.initialize();
+  console.log("initialized database");
+  await electionResults.storeAllResults(electionPath, db);
+  console.log("stored results from api");
+  process.exit(0);
+}
